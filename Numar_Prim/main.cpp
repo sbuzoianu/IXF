b66b0b479eb83d093191b735b30b05264d495a7f
Buzoianu Stefan <buzoianu.stefan@gmail.com>
2021-10-15 12:41:50 +0000

    Proiect numere prime

---
+++ Numar_Prim/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i,nr=0;
+    n=18;
+
+    for (i=1; i<=n; i++)
+
+        if (n%i==0)
+            nr++;
+
+    if (nr==2)
+        cout<<"DA";
+        else
+            cout<<"NU";
+
+    return 0;
+}
